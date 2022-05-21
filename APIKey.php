5dfdca6b41196cc8466da6b2eec9a61a7add6dff
zuhairzuberi <60561492+zuhairzuberi@users.noreply.github.com>
2022-05-20 21:06:29 -0400

    Create APIKey.php

---
+++ APIKey.php
@@ -0,0 +1,5 @@
+<?php
+  /* Created this as a seperate file for security purposes, possible to keep this away from console log as this
+  should not be public whereas other pieces of code can be. */
+	define ( 'FN_API_KEY', 'Insert your API Key here');
+?>
