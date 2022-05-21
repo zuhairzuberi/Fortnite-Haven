3af142633b2af71c4639a96781eafbffb643ff70
zuhairzuberi <60561492+zuhairzuberi@users.noreply.github.com>
2022-05-20 21:03:01 -0400

    Create itemshop.php

---
+++ itemshop.php
@@ -0,0 +1,19 @@
+<?php
+	// get our functions so we can use them!
+	require_once( 'functions.php' );
+
+	// validate and get date
+	$date = getStoreDate();
+
+	// get the items sorted
+	$storeData = getStoreSortedData( $date );
+?>
+<!DOCTYPE html>
+<html>
+<head>
+		<title>Test</title>
+	</head>
+	<body>
+		<h1>Item Shop resets at 8 PM EST</h1>
+	</body>
+</html>
