2c8b9a2576a73a7fcb0f12f785eeee2e5404bb1d
Zuhair Zuberi <60561492+zuhairzuberi@users.noreply.github.com>
2022-06-11 18:22:10 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,34 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Item Shop || Fortnite Haven</title>
+		<title>Fortnite Item Shop</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+		<style>
+			@font-face {
+			  	font-family: 'Fortnite';
+			  	src: url( 'BurbankBigCondensed-Bold.otf' );
+			}
+
+			.rarity-fine {
+				background: #fb9625;
+			}
+
+			.rarity-quality {
+				background: #7907a5;
+			}
+
+			.rarity-sturdy {
+				background: #3dc7ff;
+			}
+
+			.rarity-handmade {
+				background: #5bad03;
+			}
+
+			a {
+				color: #e3e3e3;
+			}
+		</style>
 	</head>
-	<body>
-		<h1>Item Shop resets at 8 PM EST on </h1>
-	</body>
 </html>
